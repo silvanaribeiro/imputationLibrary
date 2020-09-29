863f5b324ac117c0a1a8082a1534baa26c0aef12
smribei <SMRIBEI@corp.ad.emb>
2020-09-29 15:35:18 -0300

    making sure only past values are used to input test data

--- imputationLibrary/forwardFilling.py
+++ imputationLibrary/forwardFilling.py
@@ -7,9 +7,7 @@ def inputTestData(test_df, training_df, ignore_index=False):
     for index, row in test_df.iterrows():
         training_df = training_df.append(row, ignore_index=ignore_index)
         if (len(row)-row.count())>=1:
-            print("antes",training_df)
             training_df = training_df.ffill()
-            print("depois",training_df)
     return training_df[-test_df.shape[0]:]
 
 def input(df_training, df_test):
