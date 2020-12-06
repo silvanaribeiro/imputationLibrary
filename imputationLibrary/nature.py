c097153551702b1d0ae5d3cee8816f6e0ada3376
Silvana Mara Ribeiro <silvana.mribeiro@yahoo.com.br>
2020-12-06 15:49:07 -0300

    seasonal by acc

--- imputationLibrary/nature.py
+++ imputationLibrary/nature.py
@@ -2,9 +2,10 @@ from imputationLibrary import decompose
 from scipy import signal
 from scipy.signal import find_peaks
 from scipy.fft import fft, fftshift
-from sklearn import preprocessing
+from sklearn import preprocessing # maybe use StandardScaler?
 import numpy as np
 import pandas as pd
+import math
 
 def isWhiteNoise(ts):
     normalized = preprocessing.scale(np.array(ts.fillna(0)))
@@ -14,26 +15,27 @@ def isWhiteNoise(ts):
     lower_limit = -2/np.sqrt(len(ts))
     result = (np.logical_or(corr>upper_limit, corr<lower_limit))
     count = len(result[result==True])
-    print(lower_limit, upper_limit, count, (0.05*len(ts)*2), len(ts))
+   #print(lower_limit, upper_limit, count, (0.05*len(ts)*2), len(ts))
     return count <= (0.05*len(ts)*2)
 
 def isSeasonal(ts):
-    normalized = preprocessing.scale(np.array(ts.fillna(0)))
-    normalized = pd.DataFrame(normalized, index = ts.index)
-    N=normalized.shape[0]
-    result = decompose.decompose(normalized)
-    yf = fft(result.seasonal)
-    yplot = fftshift(yf)
-    result = 1.0/N * np.abs(yplot)
-    peaks, info = find_peaks(result, height=0.002)
-    print(peaks, info)
-    peaks = peaks.tolist()
-    for peak in peaks:
-        if peak <= (N/2 + int(N*.002)) and peak >= N/2 - int(N*.002):
-            peaks.remove(peak)
-    if len(peaks) >=2 and not isWhiteNoise(ts):
-        return True
-    return False
+    if not isWhiteNoise(ts):
+        normalized = preprocessing.scale(np.array(ts.fillna(0)))
+        normalized = pd.DataFrame(normalized, index = ts.index)
+        result = decompose.decompose(ts)
+        corr = signal.correlate(result.trend, result.trend, mode='full')
+        corr = corr/len(ts)
+        peaks, info = find_peaks(corr, height=2/np.sqrt(len(ts)))
+        peaks = peaks.tolist()
+        if len(peaks) < 2:
+            return(False)
+        first_value = peaks[1]-peaks[0]
+        for i in range(0, len(peaks)-1):
+            if peaks[i+1]-peaks[i] > first_value + 2 or peaks[i+1]-peaks[i] < first_value - 2:
+                return(False)
+        return(True)
+    return(False)
+
 
 def isTrended(ts):
     result = decompose.decompose(ts)
