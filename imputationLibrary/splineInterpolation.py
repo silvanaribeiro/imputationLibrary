89870ff7c7e4bd07e059ba814a7d92555648003a
Silvana Mara Ribeiro <silvana.mribeiro@yahoo.com.br>
2021-05-23 12:18:17 -0300

    fixing k for spline interpolation

--- imputationLibrary/splineInterpolation.py
+++ imputationLibrary/splineInterpolation.py
@@ -1,8 +1,8 @@
 from scipy import interpolate
 import numpy as np
 
-def inputData(x, y, x_new, derivate_order=0):
-    tck = interpolate.splrep(x, y, s=derivate_order)
+def inputData(x, y, x_new, derivate_order=0, k = 3):
+    tck = interpolate.splrep(x, y, s=derivate_order, k=k)
     y_new = interpolate.splev(x_new, tck, der=0)
     return y_new
 
@@ -10,9 +10,17 @@ def inputTrainingData(df, derivate_order=0):
     training_df = df.copy()
     for col, data in training_df.iteritems():
         complete_df = training_df[col].dropna()
+        if len(complete_df) == 1:
+            k = 0
+        elif len(complete_df) == 2:
+            k = 1
+        elif len(complete_df) == 3:
+            k = 2
+        else:
+            k = 3
         x_new = np.setdiff1d(training_df.index, complete_df.index)
-        if(len(x_new)>0):
-            y_new = inputData(complete_df.index.values.tolist(), complete_df.iloc[:].values.tolist(), x_new.tolist(), derivate_order)
+        if(len(x_new.tolist())>0) and k > 0:
+            y_new = inputData(complete_df.index.values.tolist(), complete_df.iloc[:].values.tolist(), x_new.tolist(), derivate_order, k)
             for i in range(0, len(x_new)):
                 training_df.at[x_new[i], col] = y_new[i]
     return training_df
